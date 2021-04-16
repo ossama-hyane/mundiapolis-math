385a51eead8e142b0ed98126ce9fc9281c70e5b6
ossama hyane <78233494+ossama-hyane@users.noreply.github.com>
2021-04-16 11:59:56 +0000

    Update poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,1 +1,53 @@
 
+#!/usr/bin/env python3
+""" programe pour coulculer poisson distribution """
+
+
+class Poisson:
+    """ poisson class """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ poisson class constructor """
+
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            total = 0
+            for i in data:
+                total = total + i
+            self.lambtha = float(total / len(data))
+
+    def pmf(self, k):
+        """focntion pour calculer pmf distribution"""
+
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        if self.lambtha is not None:
+            total = 1
+            for i in range(1, k + 1):
+                total = total * i
+            e = 2.7182818285
+            return ((e ** (- self.lambtha)) * ((self.lambtha) ** k)) / total
+
+    def cdf(self, k):
+        """fonction pour calculer cdf distribution"""
+
+        if type(k) != int:
+            k = int(k)
+        if k < 0:
+            return 0
+        if self.lambtha is not None:
+            total = 0
+            for i in range(0, k + 1):
+                total = total + self.pmf(i)
+            return total
